dac8a2a7ea535a75a1d586ce9d5905ffb7bc28a9
Vlad P <vladisslav@inbox.ru>
2020-06-20 17:46:42 +0300

    6-port switch support by default

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/vlan.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/vlan.lua
@@ -69,7 +69,8 @@ m.uci:foreach("network", "switch",
 					{ num = 2, label = "Port 3" },
 					{ num = 3, label = "Port 4" },
 					{ num = 4, label = "Port 5" },
-					{ num = 5, label = "CPU (eth0)", tagged = false }
+					{ num = 5, label = "Port 6" },
+					{ num = 6, label = "CPU (eth0)", tagged = false }
 				}
 			}
 		end
